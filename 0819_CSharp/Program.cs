90c9ed13d9d1c0c023d3eb1659c61b079cc62079
ubuntuHan <wooyeophan@gmail.com>
2021-08-19 13:02:10 +0900

    8월 19일 4교시 C# 수업 코드 커밋

---
+++ 0819_CSharp/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq; 
+
+namespace _0819_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 4교시
+            // 아 이 전껄 지웠
+            // Console.WriteLine(int.MaxValue);
+            // Console.WriteLine(int.MinValue);
+
+            // int A = 10;
+            // int B = 5;
+            // System.Console.WriteLine((A + B) / 2.0f);
+
+            // System.Console.WriteLine('s' + 0);
+
+            // int num = 100;
+            // System.Console.WriteLine((char)num);
+
+            // int a = 100;
+            // System.Console.WriteLine(Convert.ToString(a, 2));
+            // Convert.Tostring(숫자, 진법);
+            // 진법에 2 또는 8, 16 을 넣을 수 있다.
+
+            // System.Console.WriteLine(((84 + 63 + 70) / 3.0).ToString("0.00"));
+            // // Tostring("0.00") => 소수점 변환 가능
+            // System.Console.WriteLine("{0:0.00}", 10 / 3.0f);
+            // // {0::0.00} 도 가능함
+
+            // DateTime past = new DateTime(2000, 1, 1);
+            // TimeSpan ts = DateTime.Now - past;
+            // System.Console.WriteLine((int)ts.TotalDays);
+            // TimeSpan => 말 그대로 지나간시간 무튼 그런것
+
+            // string s = "of the people, by the people, for the people";
+            // string[] strings = s.Split(", ");
+            // foreach (var sa in strings)
+            // {
+            //     System.Console.WriteLine(sa);
+            // }
+
+            // while(true)
+            // {
+            //     if(int.TryParse(Console.ReadLine(), out int res))
+            //     {
+            //         System.Console.WriteLine(res);
+            //         break;
+            //     }
+            //     else
+            //     {
+            //         System.Console.WriteLine("다시 입력하세요.");
+            //     }
+            // }
+
+            // || 만 있을시 앞에 true 라면 뒤는 아예 연산하지 않음
+
+            #endregion
+        }
+    }
+}
