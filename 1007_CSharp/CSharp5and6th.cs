6313735c1277e0c93af222ce4e4868514c3cd960
HanUseArch <wooyeophan@gmail.com>
2021-10-07 15:39:39 +0900

    10월 7일 C# 커밋

--- 1007_CSharp/CSharp5and6th.cs
+++ 1007_CSharp/CSharp5and6th.cs
@@ -1,8 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace _1007_CSharp
 {
+
+    public static class IntergerExtension
+    {
+        public static int Square(this int myInt)
+        {
+            return myInt * myInt;
+        }
+
+        public static int Power(this int myInt, int exponent)
+        {
+            int result = myInt;
+            for (int i = 1; i < exponent; ++i)
+            {
+                result = result * myInt;
+            }
+
+            return result;
+        }
+
+        public static void ForEach<T>(this IEnumerable<T> list, Action<T> action)
+        {
+            foreach (T item in list)
+            {
+                action(item);
+            }
+        }
+    }
+
     class Program
     {
         #region 5교시
@@ -122,6 +151,78 @@ namespace _1007_CSharp
 
 
             #endregion 5교시
+
+            #region 6교시
+            // var output = from p in arrProfile
+            //              orderby p.Height
+            //              group p by p.Height < 170 into g
+            //              select new
+            //              {
+            //                  GroupKey = g.Key == true ? "170미만" : "170이상",
+            //                  Count = g.Count(),
+            //                  Max = g.Max(profile => profile.Height),
+            //                  Min = g.Min(profile => profile.Height),
+            //                  Average = g.Average(profile => profile.Height)
+            //              };
+            // output.ToList().ForEach(e =>{
+            //     System.Console.WriteLine("{0} {1} {2} {3} {4}", e.GroupKey, e.Count, e.Max, e.Min, e.Average);
+            // });
+
+            // var arr = new int[100];
+
+            // for (var i = 0; i < arr.Length; ++i)
+            // {
+            //     arr[i] = i * i;
+            // }
+            // foreach (var item in arr)
+            // {
+            //     System.Console.WriteLine(item);
+            // }
+            // var result = from i in Enumerable.Range(0, 100) // 0부터 99까지의 데이터를 만듬
+            //              select i * i;
+            // foreach (var item in result)
+            // {
+            //     System.Console.WriteLine(item);
+            // }
+            // result.ToList().ForEach(e => System.Console.WriteLine(e));
+            // result.ForEach(e => System.Console.WriteLine(e));
+
+            // int a = 2;
+            // System.Console.WriteLine(a.Square());
+            // System.Console.WriteLine(a.Power(10));
+
+            // var sheet = new[] {
+            //     new List<string>() {"a", "10", null, "30"},
+            //     new List<string>() {"b", "10", "50", "40"},
+            //     new List<string>() {"c", "20", "60", "60"},
+            //     new List<string>() {"a", "30", "40", "70"}
+            // }
+
+            // var result = sheet
+            //     .Where(w => x[0] == "a")
+            //     .FirstOrDefault()
+            //     .
+
+            int a = 0;
+            var list = new int[] { 10, 20, 30, 40, 45, 50 };
+            var result = list
+                .Where(x =>
+                {
+                    System.Console.WriteLine($"[Where] {x}"); ++a;
+                    return x > 30;
+                })
+                .Select(x =>
+                {
+                    System.Console.WriteLine($"[Select] {x}"); ++a;
+                    return x * x;
+                });
+
+
+            //System.Console.WriteLine(string.Join(",", result));
+            System.Console.WriteLine(a);
+
+            #endregion
+
 
 
 
